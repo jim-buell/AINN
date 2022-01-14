8a345ae892f1d41f1c04c2e3c90c8466e89322e8
jim-buell <jbuell@ideo.com>
2022-01-14 21:51:35 +0900

    re-did alignment using boxes

--- main.py
+++ main.py
@@ -1,11 +1,114 @@
+#Infinite Scroll 2.0
+
+from newsapi import NewsApiClient
+import nltk
+from nltk.corpus import stopwords
+from nltk.corpus import wordnet as wn
+from nltk.tokenize import word_tokenize, sent_tokenize
+from itertools import groupby
 import random
 from guizero import App, TextBox, Text, Picture, Box
 import time
 
+#global variables that persist to facilitate GUI display refresh
 mainStr = ""
 counter = 0
 dispStr = ""
 blinkTime = 0
+
+def grabNewHeadlines():
+	# Init
+	newsapi = NewsApiClient(api_key='2a547104c0d14649850d41b220871320')
+	headlineList = []
+	
+	# Grab headlines from NewsAPI 
+	sourceName = ['the-washington-post', 'CNN', 'BBC-news']
+	for item in sourceName: 
+		top_headlines = newsapi.get_top_headlines(sources='{}'.format(item))
+
+		# Format the headlines and remove extra data 
+		Headlines = top_headlines['articles']
+		if Headlines:
+				for articles in Headlines:
+					#b = articles['title'][::-1].index("-")
+					if "news" in (articles['title']).lower():
+						headlineList.append(f"{articles['title']}.")
+					else:
+						headlineList.append(f"{articles['title']}.")
+	print(headlineList) 
+	
+	#append headlines to master file
+	f = open("masterHeadlines.txt", "a")
+	for element in headlineList:
+		f.write(element + "\n")
+		#print(element)
+	else:
+		f.close()
+	
+	#overwrite new headlines to new headline file
+	headlineStrs = " "
+	f = open("newHeadlines.txt", "w")
+	for element in headlineList:
+		f.write(element + "\n")
+		headlineStrs += element
+		headlineStrs += " "
+	else:
+		f.close()
+	return headlineStrs
+
+#categorizes words in headlines into parts of speech and saves them to individual files
+def sortAndStore(part):
+	
+	typeList = []
+	#open the file with the Headlines and put them in a str
+	File = open(r"newHeadlines.txt", "r")
+	headlineStrs = ""
+	Lines = File.readlines()
+	for item in Lines:
+		headlineStrs = headlineStrs + " " + item.strip()
+	#print(headlineStrs)
+	stop_words = set(stopwords.words('english'))
+	tokenized = sent_tokenize(headlineStrs)
+	#
+	for i in tokenized:
+		      
+		#Word tokenizers is used to find the words 
+		#and punctuation in a string
+		wordsList = nltk.word_tokenize(i)
+  
+		#removing stop words from wordList
+		wordsList = [w for w in wordsList if not w in stop_words] 
+  
+		# Using a Tagger. Which is part-of-speech 
+		tagged = nltk.pos_tag(wordsList)
+		
+		groups = groupby(tagged, key=lambda x: x[1])
+		typeNames = [[w for w,_ in words] for tag,words in groups if tag=="{}".format(part)]
+		#if part == "NNPq":
+		#	typeNames = [" ".join(name) for name in typeNames if len(name)>=2]
+		#else:
+		typeNames = [" ".join(name) for name in typeNames if len(name)>=0] 
+		typeList += typeNames
+	
+	overWrite = "w"
+	f = open("words/{}.txt".format(part), "{}".format(overWrite))
+	for element in typeList:
+		newStr = element.strip("'")
+		f.write(newStr + " \n")
+	else:
+		f.close()
+	#print("{}".format(part) + ":", typeList)
+	
+#this function passes every part of speech to the main sortAndStore function	
+def getAllTypes():
+	typeList = ["JJ", "JJR", "JJS", "NN", "NNS", "NNP", "NNPS", "PDT", "RB", "RBR", "RBS", "RP", "VB", "VBG", "VBD", "VBN", "VBP", "VBZ"]	
+	for item in typeList:
+		sortAndStore("{}".format(item))
+
+def fetchNew():
+	grabNewHeadlines()
+	getAllTypes()
+	print("Fetched new headlines.")
 
 #gets a random word from a list of specified word types. Pass a str indicating the 
 #part of speech to get that type of word. 
@@ -74,28 +177,36 @@ def updateText():
 app = App(title="Infinite Scroll 2.0", bg = "#000000", layout = "grid", width = 640, height = 480)
 
 #sets the logo
-logo = Picture(app, image="logo.png", grid = [0, 1])
+logo = Picture(app, image="logo.png", grid = [1, 1])
 logo.tk.config(bd = 0, cursor = "none")
+logo.align = "left"
 
 #main text display 
-displayText = TextBox(app, text = "", multiline = True, grid = [0, 3])
+displayText = TextBox(app, text = "", multiline = True, grid = [1, 3])
 
 #textBox properties 
 displayText.font = "akkurat mono"
 displayText.text_color = "#00ff00"
 displayText.text_size = 50
 displayText.align = "left"
-displayText.tk.config(cursor = "none", bd = 50, highlightbackground = "#000000")
+displayText.tk.config(cursor = "none", highlightbackground = "#000000")
 displayText.borderwidth = 0
 displayText.height = 8
 displayText.width = 16
 
 #padding for logo
-top_pad = Box(app, align = "left", height = 50, width = 5, grid = [0, 0])
-bottom_pad = Box(app, align = "left", height = 1, width = 5, grid = [0, 2])
+top_pad = Box(app, align = "left", height = 30, width = 5, grid = [1, 0])
+bottom_pad = Box(app, align = "left", height = 30, width = 5, grid = [1, 2])
+left_pad = Box(app, align = "left", height = 30, width = 30, grid = [0, 0])
 
 #calls updateText repeatedly in the app loop — gets new letters to pass to the GUI
 app.repeat(200, updateText)
+
+#gets new headlines every 2 hours
+app.repeat(7200000, fetchNew)
+
+#gets updated news database
+#fetchNew()
 
 #sets the initial sentence 
 typeSen()
